508241da382f4ff1ce1470b47c46ce1f14899fb1
Zennisch <zennisch@gmail.com>
2025-11-24 19:23:17 +0900

    create: project & basic image config

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for comfyui_zns_utils."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Zennisch"""
+__email__ = "zennisch@gmail.com"
+__version__ = "0.0.1"
+
+from .src.comfyui_zns_utils.nodes import NODE_CLASS_MAPPINGS
+from .src.comfyui_zns_utils.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
---
+++ src/comfyui_zns_utils/basic_image_config.py
@@ -0,0 +1,75 @@
+class BasicImageConfig:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "base_width": ("INT", {
+                    "default": 512,
+                    "min": 64,
+                    "max": 4096,
+                    "step": 64,
+                    "display": "number"
+                }),
+                "base_height": ("INT", {
+                    "default": 512,
+                    "min": 64,
+                    "max": 4096,
+                    "step": 64,
+                    "display": "number"
+                }),
+                "scale_by": ("FLOAT", {
+                    "default": 1.0,
+                    "min": 0.1,
+                    "max": 4.0,
+                    "step": 0.01,
+                    "round": 0.01,
+                    "display": "number"
+                }),
+                "batch_size": ("INT", {
+                    "default": 1,
+                    "min": 1,
+                    "max": 16,
+                    "step": 1,
+                    "display": "number"
+                }),
+                "flip_aspect_ratio": ("BOOLEAN", {
+                    "default": False,
+                }),
+            }
+        }
+
+    CATEGORY = "Configuration"
+
+    RETURN_TYPES = (
+        "INT",  # BASE_WIDTH
+        "INT",  # BASE_HEIGHT
+        "INT",  # SCALED_WIDTH
+        "INT",  # SCALED_HEIGHT
+        "INT",  # BATCH_SIZE
+    )
+
+    RETURN_NAMES = (
+        "BASE_WIDTH",
+        "BASE_HEIGHT",
+        "SCALED_WIDTH",
+        "SCALED_HEIGHT",
+        "BATCH_SIZE",
+    )
+
+    FUNCTION = "build"
+
+    def build(self, base_width, base_height, scale_by, batch_size, flip_aspect_ratio):
+        scaled_width = int(base_width * scale_by)
+        scaled_height = int(base_height * scale_by)
+
+        if flip_aspect_ratio:
+            base_width, base_height, scaled_width, scaled_height = (
+                base_height,
+                base_width,
+                scaled_height,
+                scaled_width,
+            )
+        return base_width, base_height, scaled_width, scaled_height, batch_size
